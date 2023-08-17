e0537a93ddd54dfc5ea7397353e7987b36f61b16
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2023-08-17 18:40:15 -0400

    setup kafka cluster

--- infra/setup_kafka_cluster.py
+++ infra/setup_kafka_cluster.py
@@ -11,20 +11,23 @@ sys.path.insert(0, parent_directory)
 
 from app.core.exceptions.client_exception import ClientException
 
-api_key = os.environ['UPSTASH_API_KEY']
-email =  os.environ['EMAIL_ADDRESS']
-
-def create_kafka_cluster() -> str:
-    data = '{"name":"newyeti-prod-cluster1","region":"us-east-1","multizone":true}'
-    response = requests.post('https://api.upstash.com/v2/kafka/cluster', data=data, auth=(email, api_key))
+def handle_error(response: requests.Response):
     if response.status_code == 400:
-        raise ClientException(response.content)
+        raise ClientException(response.content.decode("utf-8"))
+    elif response.status_code == 401:
+        raise ClientException(json.loads(response.content))
+
+def create_kafka_cluster(auth: tuple) -> str:
+    data = '{"name":"newyeti-prod-cluster1","region":"us-east-1","multizone":true}'
+    response = requests.post('https://api.upstash.com/v2/kafka/cluster', data=data, auth=auth)
+    handle_error(response)
     
     response_data = json.loads(response.content)
     return response_data['cluster_id']
 
-def get_cluster_id() -> str:
-    response = requests.get('https://api.upstash.com/v2/kafka/clusters', auth=(email, api_key))
+def get_cluster_id(auth: tuple) -> str:
+    response = requests.get('https://api.upstash.com/v2/kafka/clusters', auth=auth)
+    handle_error(response)
     response_data = json.loads(response.content)
     if response_data is not None and len(response_data) > 0:
         cluster_data = response_data[0]
@@ -32,24 +35,46 @@ def get_cluster_id() -> str:
         return cluster_id
     return ""
 
-def create_kafka_topic():
+def create_kafka_topic(auth: tuple, cluster_id: str, topic_name: str) -> str:
+    data = {
+        "name": topic_name,
+        "partitions": 1,
+        "retention_time": 1234,
+        "retention_size": 4567,
+        "max_message_size": 8912,
+        "cleanup_policy": "delete",
+        "cluster_id": cluster_id
+        }
+    
+    response = requests.post('https://api.upstash.com/v2/kafka/topic', data=json.dumps(data), auth=auth)
+    handle_error(response)
+    response_data = json.loads(response.content)
+    print(response_data)
+    return ""
+    
+def list_kafka_topics(auth: tuple, cluster_id: str) -> None:
     pass
 
-def list_kafka_topics():
+def create_kafka_connector(auth: tuple):
     pass
 
-def create_kafka_connector():
-    pass
-
-def get_kafka_connector():
+def get_kafka_connector(auth: tuple):
     pass
 
 def main():
+    api_key = os.environ['UPSTASH_API_KEY']
+    email =  os.environ['EMAIL_ADDRESS']
+    auth=(email, api_key)
+    
     try:
-        cluster_id = get_cluster_id()
+        cluster_id = get_cluster_id(auth)
         if cluster_id is None or cluster_id == "":
             cluster_id = create_kafka_cluster()
         print(f"cluster_id: {cluster_id}")
+        
+        create_kafka_topic(auth=auth, cluster_id=cluster_id, topic_name="newyeti.source.teams.v1")
+        
+        
     except ClientException as e:
         print(e)
 
