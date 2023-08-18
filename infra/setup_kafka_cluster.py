c13a25ef7a43d3e2971b203de19ff633f73313b4
Sachindra Maharjan <sachindra.maharjan@outlook.com>
2023-08-18 10:20:19 -0400

    setup kafka cluster

--- infra/setup_kafka_cluster.py
+++ infra/setup_kafka_cluster.py
@@ -3,6 +3,7 @@ import json
 import os
 import requests
 import sys
+import logging
 
 # Add the parent directory (app) to sys.path
 current_directory =  os.path.abspath(os.path.dirname(__file__))
@@ -10,6 +11,10 @@ parent_directory = os.path.abspath(os.path.join(current_directory, ".."))
 sys.path.insert(0, parent_directory)
 
 from app.core.exceptions.client_exception import ClientException
+
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('infra')
 
 def handle_error(response: requests.Response):
     if response.status_code == 400:
@@ -29,6 +34,7 @@ def get_cluster_id(auth: tuple) -> str:
     response = requests.get('https://api.upstash.com/v2/kafka/clusters', auth=auth)
     handle_error(response)
     response_data = json.loads(response.content)
+    logging.debug(response_data)
     if response_data is not None and len(response_data) > 0:
         cluster_data = response_data[0]
         cluster_id = cluster_data['cluster_id']
@@ -36,24 +42,35 @@ def get_cluster_id(auth: tuple) -> str:
     return ""
 
 def create_kafka_topic(auth: tuple, cluster_id: str, topic_name: str) -> str:
+    topics = list_kafka_topics(auth=auth, cluster_id=cluster_id)
+    
+    for topic in topics:
+        if topic_name == topic['topic_name']:
+            raise ClientException(f"Topic {topic_name} already exists in the cluster.")
+        
     data = {
         "name": topic_name,
-        "partitions": 1,
-        "retention_time": 1234,
-        "retention_size": 4567,
-        "max_message_size": 8912,
+        "partitions": 2,
+        "retention_time": 604800000,
+        "retention_size": 268435456,
+        "max_message_size": 1048576,
         "cleanup_policy": "delete",
         "cluster_id": cluster_id
         }
     
     response = requests.post('https://api.upstash.com/v2/kafka/topic', data=json.dumps(data), auth=auth)
     handle_error(response)
+    logging.debug(response_data)
     response_data = json.loads(response.content)
-    print(response_data)
-    return ""
+    return response_data['topic_id']
+
     
 def list_kafka_topics(auth: tuple, cluster_id: str) -> None:
-    pass
+    response = requests.get(f'https://api.upstash.com/v2/kafka/topics/{cluster_id}', auth=auth)
+    handle_error(response=response)
+    response_data = json.loads(response.content)
+    logging.debug(response_data)
+    return response_data
 
 def create_kafka_connector(auth: tuple):
     pass
@@ -70,13 +87,13 @@ def main():
         cluster_id = get_cluster_id(auth)
         if cluster_id is None or cluster_id == "":
             cluster_id = create_kafka_cluster()
-        print(f"cluster_id: {cluster_id}")
+        logging.info(f"cluster_id: {cluster_id}")
         
         create_kafka_topic(auth=auth, cluster_id=cluster_id, topic_name="newyeti.source.teams.v1")
         
         
     except ClientException as e:
-        print(e)
+        logging.error(e)
 
 if __name__ == "__main__":
     main()
